10512cd3d53fddfd36494c88cd9374dee0c07a00
RyanPrata01 <nayr.saena212@gmail.com>
2023-06-22 11:19:29 +0700

    base file

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login() {
+        return view('auth.login');
+    }
 }
